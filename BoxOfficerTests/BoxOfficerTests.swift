6877faa87bea9d2c50df9ebe5479272f973e7a91
Nick <directr441@gmail.com>
2026-01-12 20:49:49 -0600

    Initial Commit

---
+++ BoxOfficerTests/BoxOfficerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOfficerTests.swift
+//  BoxOfficerTests
+//
+//  Created by Nick Spiro on 10/13/25.
+//
+
+import Testing
+@testable import BoxOfficer
+
+struct BoxOfficerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
